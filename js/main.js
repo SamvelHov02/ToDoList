3d5e4249bd76d3f3a92bfe8737dcacb0806a2387
Samvel Hovhannisyan <samvelh@kth.se>
2024-01-28 15:54:50 +0100

    Creates Custom tasks

--- js/main.js
+++ js/main.js
@@ -10,14 +10,30 @@ p.addEventListener("click", function () {
 const form = document.querySelector(".input-form");
 const text = document.querySelector(".input-text");
 
+// Event Listener for task submition 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let todos = localStorage.getItem("tasks");
     let newToDos = todos.split(',');
     newToDos.push(text.value);
     localStorage.setItem("tasks", newToDos);
+    createNewTask(text.value);
     text.value = "";
 });
 
 const taskInput = document.getElementById("task");
 const taskContent = taskInput.content;
+
+// add Tasks to document
+const createNewTask = (text) => {
+    const taskList = document.querySelector(".task-list");
+    let taskLi = document.createElement("li");
+    taskLi.className = `task-li-element-${text}`;
+    taskLi.appendChild(document.createTextNode(text));
+    taskList.appendChild(taskLi);
+    console.log(taskLi);
+}
+
+const deleteTask = (className) => {
+
+}
